30b5a2ac3f79dc04f6f3e46add7d62d28a44f889
Elisha Bere <elishabere4@gmail.com>
2019-10-20 16:28:48 +0200

    css changes

--- src/Shuttles.js
+++ src/Shuttles.js
@@ -10,34 +10,39 @@ function ChooseShuttle(){
     return(
         <div className="section" id="shuttle">
 				    <div className="row" >
-
+                    <Link to="/rentcar">
+                        <span className="select-arrow"></span>
+                        <h1 className="back">BACK</h1>
+                    </Link>
 					    <div className="booking-form">
+                        
 						    
                                 <div className="form-group car" >
+                               
                                     <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
                                         <div class="carousel-inner" role="listbox">
                                             <div class="carousel-item active choose">
                                                 <div className="col-md-12">
-                                                    <div className="card h-100">
+                                                    <div className="card h-100 shuttlecard">
                                                         <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""></img></a>
                                                         <div className="card-body">
                                                         <h4 className="card-title">
-                                                            <a href="#">Space Shuttle 1</a>
+                                                            <a className="shuttle_title">Space Shuttle 1</a>
                                                         </h4>
                                                         <h5>$24.99</h5>
                                                         <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                                                         </div>
                                                         <div className="card-footer">
-                                                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                                                        <button className="btn btn-secondary" style={{float:"right"}}>Select</button>
+                                                        <small className="text-muted ">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                                        <button className="select_shuttle" style={{float:"right"}}>Select</button>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="carousel-item">
                                                 <div className="col-md-12 choose">
-                                                        <div className="card h-100">
-                                                            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""></img></a>
+                                                        <div className="card h-100 shuttlecard">
+                                                            <a ><img className="card-img-top" src="http://placehold.it/700x400" alt=""></img></a>
                                                             <div className="card-body">
                                                             <h4 className="card-title">
                                                                 <a href="#">Car Two</a>
@@ -47,15 +52,15 @@ function ChooseShuttle(){
                                                             </div>
                                                             <div className="card-footer">
                                                             <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                                                            <button className="btn btn-secondary" style={{float:"right"}}>Select</button>
+                                                            <button className="select_shuttle" href="#" style={{float:"right"}}>Select</button>
                                                             </div>
                                                         </div>
                                                     </div>
                                             </div>
                                             <div class="carousel-item">
                                                 <div className="col-md-12 choose">
-                                                        <div className="card h-100">
-                                                            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""></img></a>
+                                                        <div className="card h-100 shuttlecard">
+                                                            <a ><img className="card-img-top" src="http://placehold.it/700x400" alt=""></img></a>
                                                             <div className="card-body">
                                                             <h4 className="card-title">
                                                                 <a href="#">Space Car 3</a>
@@ -65,12 +70,13 @@ function ChooseShuttle(){
                                                             </div>
                                                             <div className="card-footer">
                                                             <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                                                            <button className="btn btn-secondary" style={{float:"right"}}>Select</button>
+                                                            <button className="select_shuttle" style={{float:"right"}}>Select</button>
                                                             </div>
                                                         </div>
                                                     </div>
                                             </div>
                                         </div>
+                                    
                                         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                             <span class="sr-only">Previous</span>
